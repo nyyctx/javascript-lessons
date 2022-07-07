2db0994edc9e9af22c6491ff5cc86156dfca05ae
nyyctx <lucas_nunes_ribeiro@hotmail.com>
2022-07-07 20:25:33 -0300

    funções

---
+++ lessons/study4/script.js
@@ -0,0 +1,14 @@
+// O melhor jeito de se nomear uma função é Verbo + Substantivo
+
+let  armadura = 'malha';
+    console.log(armadura);
+
+
+function melhoraArmadura (tipo, qualidade) {
+    armadura = tipo + " " + "de" + " " + qualidade;
+};
+
+melhoraArmadura("placa", "aço");
+    console.log(armadura);
+
+    
