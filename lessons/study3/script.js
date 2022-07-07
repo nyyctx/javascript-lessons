61add6aeab2ac65f2e1920d1f9d9d0560dd295f6
nyyctx <lucas_nunes_ribeiro@hotmail.com>
2022-07-07 20:25:02 -0300

    arrays

---
+++ lessons/study3/script.js
@@ -0,0 +1,10 @@
+/* ARRAYS
+arrays são conjuntos de dados que podem ser acessados por um índice, que é o valor exato da posição 
+do dado em questão.
+
+variavel nomeVariavel = [0,1,2,3,...] */
+
+let mochila = ['caderno','celular', carteira = ['identidade', 'foto', 10.50], 'estojo'];
+
+
+console.log(mochila[2]);
