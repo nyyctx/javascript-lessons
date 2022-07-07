437fca1663632b562e92e5c702645dabeb7955ab
nyyctx <lucas_nunes_ribeiro@hotmail.com>
2022-07-07 20:23:38 -0300

    variáveis

---
+++ lessons/study1/script.js
@@ -0,0 +1,10 @@
+//tipo nomeVariavel = valor
+let varOne = 1;
+    console.log(varOne);
+    typeof(varOne);
+let stringOne = 'javascript';
+    console.log(stringOne);
+    typeof(stringOne);
+// é mais recomendado usar const, usando let como variável apenas quando for trocar valores futuramente 
+const catLives = 9;
+    console.log(catLives);
