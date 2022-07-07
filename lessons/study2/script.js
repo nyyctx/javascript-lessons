7b07d795d31a7ce678bcb4d0f931ed64ce9fb85e
nyyctx <lucas_nunes_ribeiro@hotmail.com>
2022-07-07 20:24:24 -0300

    objetos

---
+++ lessons/study2/script.js
@@ -0,0 +1,15 @@
+/* COMO FUNCIONA UM OBJETO
+
+variavel nomeVariavel = {
+    chave: valor,
+};
+
+nomeVariavel.chave */
+
+let pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Nunes Ribeiro',
+    idade: 18
+};
+
+console.log(pessoa);
