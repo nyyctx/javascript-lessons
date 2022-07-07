56403e5ed43e8cf197a985807d326586ebbbf280
nyyctx <lucas_nunes_ribeiro@hotmail.com>
2022-07-07 20:26:46 -0300

    tipos de funções

---
+++ lessons/study5/script.js
@@ -0,0 +1,13 @@
+// Realiza uma tarefa, não devolve nada
+
+function dizerNome() {
+    console.log('lucas');
+};
+
+//Realiza uma tarefa, devolve algo
+
+function multiplicarPorDois(valor) {
+    return valor*2;
+};
+result = multiplicarPorDois(10);
+    console.log(result);
