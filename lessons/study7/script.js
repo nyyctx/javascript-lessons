49dd52116bfc1b9a97e6f4ed0ccb1b6c8f0a238b
nyyctx <lucas_nunes_ribeiro@hotmail.com>
2022-07-21 14:06:48 -0300

    1. invertendo variáveis

---
+++ lessons/study7/script.js
@@ -0,0 +1,16 @@
+// Trocando e Armazenando valores das variáveis
+
+let x = 1;
+let y = 0;
+
+console.log('A variável x é:', x);
+console.log('A variável y é:', y);
+console.log('Invertendo...');
+
+let z = x; //?
+    x = y; // !
+    y = z; // ?
+
+console.log('A variável x é:', x);
+console.log('A variável y é:', y);
+
